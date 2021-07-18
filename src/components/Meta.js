524eabe6610cfe7c9bf516bc3407b77b563ae49f
Kenntoft <Jerken@ssm.se>
2021-07-18 12:05:18 +0200

    twitter etc

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -35,26 +35,26 @@ export default class Meta extends Component {
         {title && <title>{title}</title>}
         {title && <meta property="og:title" content={title} />}
         {description && <meta name="description" content={description} />}
-        {description && (
-          <meta property="og:description" content={description} />
-        )}
+        {description && <meta property="og:description" content={description} />}
         {canonicalLink && <meta property="og:type" content="website" />}
         {canonicalLink && <meta property="og:url" content={canonicalLink} />}
         {canonicalLink && <meta name="twitter:site" content={canonicalLink} />}
-        {twitterCreatorAccount && (
+        {/* {twitterCreatorAccount && (
           <meta name="twitter:creator" content={twitterCreatorAccount} />
-        )}
+        )} */}
        
         {canonicalLink && <link rel="canonical" href={canonicalLink} />}
 
         <meta property="og:locale" content="sv_SE" />
         <meta property="og:site_name" content={siteTitle} />
-        <meta name="twitter:description" content={siteDescription} />
-        <meta name="twitter:title" content={siteTitle} />
+        {description && <meta property="twitter:description" content={description} />}
+        {/* <meta name="twitter:description" content={siteDescription} /> */}
+        {/* <meta name="twitter:title" content={siteTitle} /> */}
+        {title && <meta property="twitter:title" content={title} />}
         <meta name="twitter:image" content={absoluteImageUrl} />
         <meta property="og:image:secure_url" content={absoluteImageUrl} />
         <meta property="og:image" content={absoluteImageUrl} />
-        <meta name="twitter:card" content={absoluteImageUrl} />
+        <meta name="twitter:card" content="summary" />
        
        {googleTrackingId && (
           <script
