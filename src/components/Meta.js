b302f8f1d588d1e459c29cf99b426bfd38f0b3d7
carnalito80 <kunbg_jk@hotmail.com>
2021-08-16 12:04:00 +0200

    meta scheme

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -27,6 +27,7 @@ export default class Meta extends Component {
       siteTitle,
       siteDescription,
       googleTrackingId='UA-182855928-2',
+     
       // overwrite { title, description } if in fields or fields.meta
     } = this.props
 
@@ -51,6 +52,46 @@ export default class Meta extends Component {
         <meta property="og:image" content={absoluteImageUrl} />
         <meta name="twitter:card" content="summary" />
         <meta name="google-site-verification" content="pOGe1T83kU7OVdDrqaYmK22Y-GGcWBOVH1ayh978AAM" />
+        <script type="application/ld+json">{`
+         {
+          "@context": "https://schema.org",
+          "@type": "LocalBusiness",
+          "name": "Flyttstädning Helsingborg",
+          "image": "https://xn--flyttstdning-helsingborg-wbc.nu/images/logo.png",
+          "@id": "https://xn--flyttstdning-helsingborg-wbc.nu/",
+          "url": "https://xn--flyttstdning-helsingborg-wbc.nu/",
+          "inLanguage": "sv-SE",
+          "description": "Fyttstädning Helsingborg -en pålitlig städfirma med personal du kan lita på ✔️. Vi erbjuder fasta priser och städgaranti ✔️. Boka oss eller begär en offert enkelt online eller via telefon ✔️.",
+          "telephone": "073 637 99 08",
+          "priceRange": "Från 1200kr",
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Badhusgatan 2",
+            "addressLocality": "Helsingborg",
+            "postalCode": "25221",
+            "addressCountry": "SE"
+          },
+          "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": 56.0466408,
+            "longitude": 12.6908438
+          },
+          "openingHoursSpecification": {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": [
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+              "Saturday",
+              "Sunday"
+            ],
+            "opens": "08:00",
+            "closes": "18:00"
+          } 
+        }
+        `}</script>
        {googleTrackingId && (
           <script
             async
